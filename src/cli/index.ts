f977297ca31438078c648412aab5fefd809664fa
KabAchOk <bogdan.shustik1@mail.ru>
2025-04-25 16:17:49 +0500

    feat: add logger config and restApp

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,4 +1,4 @@
-export * from './cli-application.js';
+export * from './cliApplication.js';
 export * from './commandParser.js';
 export * from './commands/help.js';
 export * from './commands/version.js';
---
+++ src/rest/index.ts
@@ -0,0 +1,1 @@
+export * from './restApplication.js';
